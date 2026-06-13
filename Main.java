a44e74050ac76ec7df31a73b3c3b909266038c5b
Benjamin Ng <ben@Benjamins-MacBook-Air-3.local>
2026-06-13 16:22:51 +0800

    Added MBC-1 support and open source boot bios. Fixed ppu and cpu bugs

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String args[]) {
         System.out.println("1. Loading ROM...");
-        Catridge catridge = new Catridge(FileParser.parse("/Users/ben/Downloads/superMario2.gb"));
+        Catridge catridge = new Catridge(FileParser.parse("rom filepath here"));
         
         System.out.println("2. Initializing Gameboy...");
         Gameboy gb = new Gameboy(catridge);
